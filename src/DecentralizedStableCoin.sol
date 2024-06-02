857d59354ec4968744ee22fb43f3c7de9ebc58f8
Abhinav Sharma <abhi5237@gmail.com>
2024-06-02 20:21:39 +0530

    minor update

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -38,9 +38,42 @@ pragma solidity ^0.8.20;
  * This is meant to be governed by DSDEngine. This is just an ERC20 implementation of the ERC20 stablecoin system.
  */
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
     constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
+
+        // Check if the balance is zero
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        // Check if balance is less than amount
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        // Super calls the 'burn' function in the parent class
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+
+        return true;
     }
 }
